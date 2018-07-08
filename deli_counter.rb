1b15f1d2e486135a1e2dd262126ae64ef03433a3
Rosa Tu <rosalina.tu@gmail.com>
2018-07-08 19:44:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,11 @@ def line(deli)
   if i < deli.length 
    deli.each.with_index do |name, i| 
      new_line << "The line is currently: #{i + 1}. #{name}"
+     new_line
   end
   else
     puts "The line is currently empty."
   end
-  new_line
 end  
 
 def take_a_number(katz_deli, name)
