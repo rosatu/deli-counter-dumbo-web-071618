fcc160e87ac35b0a245ff8c7749adeae4395ae29
Rosa Tu <rosalina.tu@gmail.com>
2018-07-08 19:22:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else 
+  i = 0
   while deli.length > 0 
    deli.each_with_index do |name, i|
-   puts "The line is currently: #{i + 1}. #{name}"
+   "The line is currently: #{i + 1}. #{name}"
+   i =+ 1
    end
   end  
 end
