a90813fc509b8d4e708a2f26c5b97c159f712155
Rosa Tu <rosalina.tu@gmail.com>
2018-07-08 19:23:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,14 @@
 katz_deli = []
 
 def line(deli)
-  if deli.length == 0
-    puts "The line is currently empty."
-  else 
   i = 0
-  while deli.length > 0 
+  while i < deli.length 
    deli.each_with_index do |name, i|
-   "The line is currently: #{i + 1}. #{name}"
+   puts "The line is currently: #{i + 1}. #{name}"
    i =+ 1
    end
+   while deli.length == 0
+    puts "The line is currently empty."
   end  
 end
 
